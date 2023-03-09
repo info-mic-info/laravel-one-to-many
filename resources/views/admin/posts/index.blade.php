1db8649d324b265dff14e3f6b6cfd2ec9b23e9e9
info-mic-info <jobsocial94@gmail.com>
2023-03-09 16:32:09 +0100

    last push

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -35,7 +35,7 @@
                         </a>
                         <a href="{{route('admin.posts.edit', $post-> slug)}}" title="Modifica post" class="btn btn-warning"><i class="fas fa-edit"></i></a>
 
-                 <form class="d-inline-block" action="{{ route('admin.posts.destroy, $post->slug)}}" method="POST">
+                 <form class="d-inline-block" action="{{ route('admin.posts.destroy', $post->slug)}}" method="POST">
                     @csrf
                     @method('DELETE')
                     <button type="submit" class="btn btn-danger">
